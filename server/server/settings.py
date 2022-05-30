e1a5628c8ceca1e44a3f6a0a399b73ed9beb9162
Vorapas <max.saparov@gmail.com>
2022-05-29 23:31:18 -0400

    settings

--- server/server/settings.py
+++ server/server/settings.py
@@ -129,13 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = '/static/'
-STATIC_CLIENT = os.path.join(os.path.dirname(BASE_DIR), 'static/')
-print(STATIC_CLIENT)
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    STATIC_CLIENT,
-]
+# Use this if you need to serve vue static files from Django
+# STATIC_CLIENT = os.path.join(os.path.dirname(BASE_DIR), 'static/')
+# STATICFILES_DIRS = [
+#     STATIC_CLIENT,
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
